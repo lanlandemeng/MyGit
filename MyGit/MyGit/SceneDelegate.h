c3405e1b1d925befacb7d94c109503546d5c4197
wangweichao <18211090765@163.com>
2020-03-27 11:46:14 +0800

    初始化工程

---
+++ MyGit/MyGit/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  MyGit
+//
+//  Created by wwchao on 2020/3/27.
+//  Copyright © 2020 wwchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ MyGit/MyGit/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MyGit
+//
+//  Created by wwchao on 2020/3/27.
+//  Copyright © 2020 wwchao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
